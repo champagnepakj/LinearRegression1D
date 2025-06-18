3309364b96182cc1bc5ca7f3327c7d9da4851b62
champagnepaki <zion.dazz@outlook.com>
2025-06-18 08:51:03 +0100

    Added working simple linear regression (1D)

--- LinearRegression1D.cpp
+++ LinearRegression1D.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 
@@ -42,8 +43,44 @@ double compute_loss(std::vector<int>& X, std::vector<int>& Y, double w, double b
 
 
 
+void compute_gradients(std::vector<int>& X, std::vector<int>& Y, double w, double b, double &dw, double &db)
+{
+
+    int n = X.size();
+    dw = 0.0;
+    db = 0.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        double y_hat = predict(X[i], w, b);
+        double error = Y[i] - y_hat;
+
+        dw += -2.0 * X[i] * error;
+        db += -2.0 * error;
+        
+    }
+
+    dw /= n;
+    db /= n;
+
+}
 
 
+
+void export_to_csv(const std::vector<int>& X, const std::vector<int>& Y, const std::vector<double>& Y_hat)
+{
+    std::ofstream file("output.csv");
+    file << "X,Y,Prediction\n";
+    for (size_t i = 0; i < X.size(); i++) {
+        file << X[i] << "," << Y[i] << "," << Y_hat[i] << "\n";
+    }
+    file.close();
+}
+
+void log_epoch(int epoch, double loss, double w, double b, std::ofstream& file)
+{
+    file << epoch + 1 << "," << loss << "," << w << "," << b << "\n";
+}
 
 
 
@@ -53,24 +90,50 @@ double compute_loss(std::vector<int>& X, std::vector<int>& Y, double w, double b
 
 int main()
 {
-    const double weight{ 1.0 };
-    const double bias{ 0.0 };
+    double weight{ 1.0 };
+    double bias{ 0.0 };
+    const double learning_rate{ 0.01 };
+    const int epochs{ 100 };
+
 
     std::vector<int> X{ 1, 2, 3, 4, 5 };
     std::vector<int> Y{ 2, 4, 6, 8, 10 }; // Actual
+    std::vector<double> Y_hat;
 
     
+
+    std::ofstream logFile("loss_log.csv");
+    logFile << "Epoch,Loss,w,b\n";
+
+
+    for (int epoch = 0; epoch < epochs; epoch++)
+    {
+        double dw;
+        double db;
+
+        compute_gradients(X, Y, weight, bias, dw, db);
+
+        weight -= learning_rate * dw;
+        bias -= learning_rate * db;
+
+        double loss{ compute_loss(X, Y, weight, bias) };
+        std::cout << "Epoch " << epoch + 1 << " | Loss: " << loss << " | w: " << weight << " | b: " << bias << "\n";
+
+        log_epoch(epoch, loss, weight, bias, logFile);
+
+
+    }
+
+    logFile.close();
+
+
     for (int i = 0; i < X.size(); i++)
     {
         double element_prediction = predict(X[i], weight, bias);
+        Y_hat.push_back(element_prediction);
         std::cout << "Input:" << X[i] << ", Actual: " << Y[i] << ", Predicted: " << element_prediction << '\n';
     }
-    
 
-    double loss{ compute_loss(X, Y, weight, bias) };
-    std::cout << "MSE Loss: " << loss << '\n';
-
-   
 
     return 0;
 }
